c85191e2b2beede0038bc04a891e12ff70fa250b
Madina <156341368+madinaabdullayeva07@users.noreply.github.com>
2024-01-11 22:32:52 +0500

    Add files via upload

---
+++ Бронприемдоктора.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class FindDoctor {
+public:
+    string Name;
+    string Adres;
+    string Problem;
+    string Doctor;
+
+    void inputDoctorDetails() {
+        cout << "Введите свое имя  : ";
+        cin >> Name;
+        cout << "Введите свой адрес: ";
+        cin >> Adres;
+        cout << "Введите свою пробдему: ";
+        cin >> Problem;
+        cout << "Выберите нужную специальность доктора: ";
+        cin >> Doctor;
+    }
+};
+
+class DepartureCity {
+public:
+    string cityName;
+
+    void inputDepartureCity() {
+        cout << "Введите город : ";
+        cin >> cityName;
+    }
+};
+
+class Client {
+public:
+    string clientName;
+
+    void inputClient() {
+        cout << "Введите ваше Имя и Фамилию: ";
+        cin >> clientName;
+    }
+};
+
+class Number {
+public:
+    double clientNumber;
+
+    void inputNumber() {
+        cout << "Ваш номер телефона: ";
+        cin >> clientNumber;
+    }
+};
+
+class Adres {
+public:
+    string Adress;
+
+    void inputAdres() {
+        cout << "Введите свой адрес: ";
+        cin >> Adress;
+    }
+};
+
+class Doctor  {
+public:
+    string professionDoctor;
+
+    void inputDoctor() {
+        cout << " Тип доктора: ";
+        cin >> professionDoctor;
+    }
+};
+int main() {
+  setlocale(LC_ALL, "Russian");
+    FindDoctor myFindDoctor;
+    myFindDoctor.inputDoctorDetails();
+
+    DepartureCity departure;
+    departure.inputDepartureCity();
+    
+    Client client;
+    client.inputClient();
+    
+    Number number;
+    number.inputNumber();
+    
+    Adres adres;
+    adres.inputAdres();
+    
+    Doctor doctor;
+    doctor.inputDoctor();
+    
+    cout << "Вы забронировали прием к врачу" << endl;
+
+    return 0;
+}
